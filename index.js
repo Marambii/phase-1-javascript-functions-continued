72725afa48749dd541a59adf3885fca0abc38e7f
Baruch Marambi Mwenda <marambib2019@gmail.com>
2023-03-15 05:35:05 +0300

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate"){
+    return`This Saturday, I want to ${activity}!`;
+}
+
+function mondayWork(expectation = "go to the office"){
+    return `This Monday, I will ${expectation}.`
+
+}
+mondayWork(expectation = "work from home")
+
+function wrapAdjective(flair = "*") {
+    return function(adjective = "special"){
+        return `You are ${flair}${adjective}${flair}!`;
+    };
+} 
+
+
+const encouragingPromptFunction = wrapAdjective("!!!");
+console.log(encouragingPromptfunction("awesome"));
+console.log(encouragingPromptFunction());
+
+
